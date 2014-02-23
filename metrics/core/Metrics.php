0a5a1db69905b64c970df67cab8e2a8e1f07e558
AfterLifeLochie <afterlifelochie@afterlifelochie.net>
2014-02-23 12:23:00 +1100

    Context closure.

---
+++ metrics/core/Metrics.php
@@ -0,0 +1,46 @@
+<?php
+class Metrics {
+	# Singleton
+	private static $instance;
+	
+	public static function instance() {
+		if (!self::$instance)
+			throw new Exception("No such singleton of Metrics.");
+		return self::$instance;
+	}
+	
+	protected $context_property;
+	protected $dpdo_dbc = array();
+	
+	public function __construct($varargContext) {
+		if ($varargContext == null)
+			throw new Exception("Can't __construct without cvarargContext param.");
+		self::$instance = this;
+		$this->context_properties = $avarargContext;
+	}
+	
+	public function run() {
+		$viewContext = null;
+		// todo: viewcontext as db_case
+		
+		$viewContext->run();
+	}
+	
+	public function getProperty($key) {
+		return $this->context_property[$key];
+	}
+	
+	public function dbc($context) {
+		if ($context == null)
+			throw new Exception("Can't create DBC closure without context name!");
+		if ($this->dpdo_dbc[$context] === null)
+			$this->dpdo_dbc[$context] = new PDO(
+				"mysql:" .
+					"host=" . $this->context_property["dbc"]["source"] . ";" .
+					"dbname=" . $context,
+				$this->context_property["dbc"]["identity"], 
+				$this->context_property["dbc"]["password"]);
+		return ($this->dpdo_dbc[$context]);
+	}
+	
+}
