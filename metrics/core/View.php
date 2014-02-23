459027020f612b533c8dff8ea2d35ca946d2b234
AfterLifeLochie <afterlifelochie@afterlifelochie.net>
2014-02-23 12:27:03 +1100

    View impl.

---
+++ metrics/core/View.php
@@ -0,0 +1,30 @@
+<?php
+class View {
+	protected $template_base = "templates/";
+	protected $vars = array();
+	protected $template_label;
+	
+	public function __construct($renderer = null, $settings = null) {
+		if ($renderer !== null)
+			$this->template_label = $renderer;
+		if ($settings !== null)
+			foreach ($settings as $keyOf => $valueOf)
+				$this->vars[$keyOf] = $valueOf;
+	}
+	
+	public function render() {
+		if (file_exists($this->template_base . $this->template_label)) {
+			include $this->template_base . $this->template_label;
+		} else
+			throw new Exception("No such template label `" . $this->template_label . "`.");
+	}
+	
+	public function __set($var, $literal) {
+		$this->vars[$var] = $literal;
+	}
+	
+	public function __get($var) {
+		return $this->vars[$var]; 
+	}
+	
+}
